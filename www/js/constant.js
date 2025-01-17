d10f12ca3233a7c9a405441d56e91e1c0f0dfa25
b4uheart <b4uheart@gmail.com>
2025-01-18 02:01:53 +0530

    fixed api url v2

--- www/js/constant.js
+++ www/js/constant.js
@@ -7,7 +7,7 @@
 let selectedFiles = []; // Files selected but not uploaded
 const Config = {
     api_url: "https://teamka.in/crm1/APIs/api_development.php",
-    domain : "https://school-project-one-steel.vercel.app/www/",
+    domain : "https://ash11109.github.io/school-project/www/",
     //domain : "http://localhost/live/www/",
     autoAssignUrl : "https://teamka.in/crm1/APIs/"
 };
