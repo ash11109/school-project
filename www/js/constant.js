78aab391fbb64a1a64a31e4cf133b52c15795315
b4uheart <b4uheart@gmail.com>
2025-01-18 01:57:53 +0530

    fixed api url

--- www/js/constant.js
+++ www/js/constant.js
@@ -5,15 +5,15 @@
 // };
 //dev
 let selectedFiles = []; // Files selected but not uploaded
-// const Config = {
-//     api_url: "https://teamka.in/crm1/APIs/api_development.php",
-//     domain : "https://school-project-one-steel.vercel.app/www/",
-//     //domain : "http://localhost/live/www/",
-//     autoAssignUrl : "https://teamka.in/crm1/APIs/"
-// };
 const Config = {
-    api_url: "http://localhost/live/APIs/api_development.php",
+    api_url: "https://teamka.in/crm1/APIs/api_development.php",
     domain : "https://school-project-one-steel.vercel.app/www/",
     //domain : "http://localhost/live/www/",
-    autoAssignUrl : "http://localhost/live/APIs/"
+    autoAssignUrl : "https://teamka.in/crm1/APIs/"
 };
+// const Config = {
+//     api_url: "http://localhost/live/APIs/api_development.php",
+//     domain : "https://school-project-one-steel.vercel.app/www/",
+//     //domain : "http://localhost/live/www/",
+//     autoAssignUrl : "http://localhost/live/APIs/"
+// };
